1a231e7fed5ae2c06d00fb5e81d9570f81ccf7bc
raushni <raushnikumari67086@gmail.com>
2024-02-16 23:40:28 +0530

    commited

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../component/Hero/Hero'
 import Popular from '../component/Popular/Popular'
+import Offers from '../component/Offers/Offers'
 
 function Shop() {
   return (
     <div>
         <Hero/>
         <Popular/>
+        <Offers/>
     </div>
   )
 }
